9810cf3131bc04f3da1f81c03b0fc905a60b3ff1
erhan1988 <erhan@streann.com>
2025-04-24 19:58:41 +0200

    Removed necessary code

--- tests/contactUs.js
+++ tests/contactUs.js
@@ -48,7 +48,6 @@ async function contactUsFirstScenario(page, action, stepNumber) {
     }
   });
 }
-
 async function contactUsSecondScenario(page, action, stepNumber) {
   await test.step(`${stepNumber}.Contact Us Second Scenario click in submit button all fields empty need to appear warning message`, async () => {
     logStep(`${stepNumber}.Contact Us Second Scenario click in submit button all fields empty need to appear warning message`);
@@ -92,76 +91,10 @@ async function checkAriaInvalid(page, field) {
 }
 
 
-
-
-
-
 module.exports = {
   contactUsFirstScenario,
   contactUsSecondScenario,
 };
-
-
-// async function checkTextExist(page, expectedTerms) {
-//   const fullText = (await page.locator('body').innerText()).toLowerCase();
-
-//   for (const term of expectedTerms) {
-//     if (fullText.includes(term.toLowerCase())) {
-//       await logSuccess(`Found word text: "${term}"`);
-//       return true; // early return if found
-//     }
-//   }
-//   throw new Error(`Could not find any of: ${expectedTerms.join(" OR ")}`);
-// }
-
-// async function privacyPolicy(page) {
-//   try {
-//     // ✅ Check and click Privacy Policy
-//     const privacyLink = page.getByRole('link', { name: /privacy policy|política de privacidad/i });
-//     await privacyLink.waitFor({ state: 'visible', timeout: 8000 });
-//     const privacyText = await privacyLink.textContent();
-//     await logSuccess(`Found Privacy Policy link: "${privacyText?.trim()}"`);
-
-//     await Promise.all([
-//       privacyLink.click(),
-//       page.waitForURL(/\/privacy/, { timeout: 10000 })
-//     ]);
-//     await logSuccess('✅ Clicked Privacy Policy and navigated to privacy page');
-//     await redirectUrl(page,'/privacy');
-
-
-//     // ✅ Wait for known content (optional)
-//     await page.waitForSelector('body', { timeout: 5000 });
-//     await page.waitForTimeout(7000);
-
-//     // ✅ Check privacy page content
-//     await checkTextExist(page, [
-//       "Privacy Policy Effective Since",
-//       "Política de Privacidad"
-//     ]);
-//   } catch (err) {
-//     logError(`❌ An error occurred in privacyPolicy: ${err.message}`);
-//     throw err;
-//   }
-// }
-
-// async function contactUs(page) {
-//   try {
-//     const contactLink = page.getByRole('link', { name: /contact us|contáctenos/i });
-//     await contactLink.waitFor({ state: 'visible', timeout: 8000 });
-//     const contactUsText = await contactLink.textContent();
-//     await logSuccess(`Found Contact Us link: "${contactUsText?.trim()}"`);
-//     await Promise.all([
-//       contactLink.click(),
-//       page.waitForURL(/\/contact-us/, { timeout: 10000 })
-//     ]);
-//     await logSuccess('✅ Clicked Contact Us and navigated to Contact Us Page');
-//     expect(page.url()).toContain('/contact-us');
-//   } catch (err) {
-//     logError(`❌ An error occurred in contactUs: ${err.message}`);
-//     throw err;
-//   }
-// }
 
 
 
